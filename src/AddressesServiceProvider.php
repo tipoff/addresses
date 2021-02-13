81535dd0b000d2b35bda7b5f90a0caf16c7c77bc
Drew Roberts <github@drewroberts.com>
2021-02-13 10:18:20 -0500

    Remove policies since not created yet

--- src/AddressesServiceProvider.php
+++ src/AddressesServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tipoff\Addresses;
 
-use Tipoff\Addresses\Policies\AddressesPolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -13,9 +12,6 @@ class AddressesServiceProvider extends TipoffServiceProvider
     public function configureTipoffPackage(TipoffPackage $package): void
     {
         $package
-            ->hasPolicies([
-                Addresses::class => AddressesPolicy::class,
-            ])
             ->name('addresses')
             ->hasConfigFile();
     }
