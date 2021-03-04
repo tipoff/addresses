5c6fa656d177a9c5d816c64e307605ca2e7eee68
Drew Roberts <github@drewroberts.com>
2021-03-04 17:23:03 -0500

    Use package classes

--- database/migrations/2021_03_04_100000_create_addresses_table.php
+++ database/migrations/2021_03_04_100000_create_addresses_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Tipoff\Addresses\Models\City;
+use Tipoff\Addresses\Models\ZipCode;
 
 class CreateAddressesTable extends Migration
 {
@@ -12,14 +14,9 @@ class CreateAddressesTable extends Migration
             $table->id();
             $table->string('address_line_1');
             $table->string('address_line_2')->nullable();
-            $table->foreignIdFor(app('city'));
-            $table->foreignIdFor(app('zip_codes'), 'zip_code');
+            $table->foreignIdFor(City::class);
+            $table->foreignIdFor(ZipCode::class);
             $table->timestamps();
-        });
-
-        Schema::table('addresses', function ($table) {
-            $table->foreign('city_id')->references('id')->on('city')->onDelete('restrict')->onUpdate('cascade');
-            $table->foreign('zip_code')->references('code')->on('zip_codes')->onDelete('restrict')->onUpdate('cascade'); // Notice primary key is 'code' and not 'id'
         });
     }
 }
