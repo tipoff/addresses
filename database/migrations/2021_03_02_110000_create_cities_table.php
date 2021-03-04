24375f37875fceef401f2f4b48c0e92157898293
devjk1 <jinsik@omniadigital.io>
2021-03-04 13:21:03 -0500

    add City Model and Migration

---
+++ database/migrations/2021_03_02_110000_create_cities_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Tipoff\Addresses\Models\State;
+
+class CreateCitiesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique()->index();
+            $table->string('title')->unique();
+            $table->string('description')->nullable();
+            $table->foreignIdFor(State::class);
+            $table->timestamps();
+        });
+    }
+}
---
+++ src/Models/City.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Addresses\Models;
+
+use Tipoff\Support\Models\BaseModel;
+use Tipoff\Support\Traits\HasPackageFactory;
+
+class City extends BaseModel
+{
+    use HasPackageFactory;
+
+    protected $casts = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($city) {
+            if (empty($city->title)) {
+                throw new \Exception('A city must have a name.');
+            }
+            if (empty($city->slug)) {
+                throw new \Exception('A city must have a slug.');
+            }
+            if (empty($city->state_id)) {
+                throw new \Exception('A city must belong to a state.');
+            }
+        });
+    }
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
+}
