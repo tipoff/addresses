48ac998c7adf856df9ca8c5d09aa76f4da0db81a
Drew Roberts <github@drewroberts.com>
2021-03-04 11:47:48 -0500

    Use package class instead of app resolver

--- database/migrations/2021_03_02_100000_create_states_table.php
+++ database/migrations/2021_03_02_100000_create_states_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Tipoff\Addresses\Models\Country;
 
 class CreateStatesTable extends Migration
 {
@@ -15,7 +16,7 @@ class CreateStatesTable extends Migration
             $table->string('abbreviation', 2)->unique();
             $table->string('description')->nullable();
             $table->string('capital')->nullable();
-            $table->foreignIdFor(app('country'));
+            $table->foreignIdFor(Country::class);
             $table->timestamps();
         });
     }
