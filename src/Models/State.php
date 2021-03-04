45464af8a6ae240a21c24007f7c4db72db49857b
drewroberts <drewroberts@users.noreply.github.com>
2021-03-04 17:00:11 +0000

    Fix styling

--- src/Models/State.php
+++ src/Models/State.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tipoff\Addresses\Models;
 
-use Tipoff\Addresses\Models\Country;
 use Tipoff\Support\Models\BaseModel;
 use Tipoff\Support\Traits\HasPackageFactory;
 
