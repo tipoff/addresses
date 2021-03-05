959a87368518c3ba99166e3f9d1c9d37d7566396
Kyle Barney <kyle@kylebarney.dev>
2021-03-04 18:56:43 -0700

    Switch to class reference until the Addresses package models are added to the support package config classmap.

--- database/migrations/2021_03_04_100000_create_addressables_table.php
+++ database/migrations/2021_03_04_100000_create_addressables_table.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Tipoff\Addresses\Models\Address;
 
 class CreateAddressablesTable extends Migration
 {
@@ -12,7 +13,7 @@ class CreateAddressablesTable extends Migration
     {
         Schema::create('addressables', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(app('addresses'))->index();
+            $table->foreignIdFor(Address::class)->index();
             $table->morphs('addressable');
             $table->string('primary_billing')->nullable();
             $table->string('primary_shipping')->nullable();
