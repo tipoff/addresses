ebf235c57b8b06ecacb9bd25512d41880f1a3617
Drew Roberts <github@drewroberts.com>
2021-03-07 15:52:55 -0500

    Add policy permissions to db migration #40

--- database/migrations/2021_03_01_100000_add_addresses_permissions.php
+++ database/migrations/2021_03_01_100000_add_addresses_permissions.php
@@ -9,12 +9,27 @@ class AddAddressesPermissions extends BasePermissionsMigration
     public function up()
     {
         $permissions = [
-            'view customers',
-            'create customers',
-            'update customers',
+            'view addresses',
+            'create addresses',
+            'update addresses',
+            'view cities',
+            'create cities',
+            'update cities',
+            'view countries',
+            'create countries',
+            'update countries',
+            'view regions',
+            'create regions',
+            'update regions',
+            'view states',
+            'create states',
+            'update states',
             'view timezones',
             'create timezones',
             'update timezones',
+            'view zip codes',
+            'create zip codes',
+            'update zip codes',
         ];
 
         $this->createPermissions($permissions);
