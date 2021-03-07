299514dd79456ff25e9ef733564f2e941779b0bb
Drew Roberts <github@drewroberts.com>
2021-03-07 15:43:11 -0500

    Add policies to package service provider

--- src/AddressesServiceProvider.php
+++ src/AddressesServiceProvider.php
@@ -4,8 +4,20 @@ declare(strict_types=1);
 
 namespace Tipoff\Addresses;
 
+use Tipoff\Addresses\Models\Address;
+use Tipoff\Addresses\Models\City;
+use Tipoff\Addresses\Models\Country;
+use Tipoff\Addresses\Models\Region;
+use Tipoff\Addresses\Models\State;
 use Tipoff\Addresses\Models\Timezone;
+use Tipoff\Addresses\Models\ZipCode;
+use Tipoff\Addresses\Policies\AddressPolicy;
+use Tipoff\Addresses\Policies\CityPolicy;
+use Tipoff\Addresses\Policies\CountryPolicy;
+use Tipoff\Addresses\Policies\RegionPolicy;
+use Tipoff\Addresses\Policies\StatePolicy;
 use Tipoff\Addresses\Policies\TimezonePolicy;
+use Tipoff\Addresses\Policies\ZipCodePolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -15,7 +27,13 @@ class AddressesServiceProvider extends TipoffServiceProvider
     {
         $package
             ->hasPolicies([
+                Address::class => AddressPolicy::class,
+                City::class => CityPolicy::class,
+                Country::class => CountryPolicy::class,
+                Region::class => RegionPolicy::class,
+                State::class => StatePolicy::class,
                 Timezone::class => TimezonePolicy::class,
+                ZipCode::class => ZipCodePolicy::class,
             ])
             ->hasNovaResources([
                 \Tipoff\Addresses\Nova\Address::class,
