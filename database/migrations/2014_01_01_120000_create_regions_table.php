36287cd6d8862ead8543527c382ca9c2b650d29c
Drew Roberts <github@drewroberts.com>
2021-03-06 23:40:04 -0500

    Change date to reorder regions migration #42

--- database/migrations/2014_01_01_120000_create_regions_table.php
+++ database/migrations/2014_01_01_120000_create_regions_table.php
@@ -10,10 +10,11 @@ class CreateRegionsTable extends Migration
     {
         Schema::create('regions', function (Blueprint $table) {
             $table->increments('id');
-            $table->foreignIdFor(app('user'), 'creator_id');
-            $table->foreignIdFor(app('user'), 'updater_id');
             $table->string('slug')->unique()->index();
             $table->string('name')->unique();
+            $table->foreignIdFor(app('user'), 'creator_id');
+            $table->foreignIdFor(app('user'), 'updater_id');
+            $table->timestamps();
         });
     }
 }
