d706a557609997acbfbb320e7763f69c6d2c1b12
devjk1 <jinsik@omniadigital.io>
2021-03-12 16:58:51 -0500

    [TGER-111] add Migration, Phone

---
+++ database/migrations/2015_01_01_170000_create_phones_table.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhonesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id();
+            $table->string('phone', 15);
+            
+            $table->foreignIdFor(app('user'), 'creator_id');
+            $table->foreignIdFor(app('user'), 'updater_id');
+            $table->timestamps();
+        });
+    }
+}
+
