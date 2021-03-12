ec555dfa3ee8c606c26154f770f1be8fe84bbefa
devjk1 <jinsik@omniadigital.io>
2021-03-12 17:02:14 -0500

    [TGER-111] add Model, Phone

---
+++ src/Models/Phone.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Addresses\Models;
+
+use Tipoff\Support\Models\BaseModel;
+use Tipoff\Support\Traits\HasPackageFactory;
+
+class Phone extends BaseModel
+{
+    use HasPackageFactory;
+
+    protected $casts = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($phone) {
+            if (empty($phone->phone)) {
+                throw new \Exception('A phone must have a phone number.');
+            }
+        });
+    }
+}
