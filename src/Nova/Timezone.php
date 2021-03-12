c1f2e52fe02788261f00cddd1e0c88b35ae2ad48
Drew Roberts <github@drewroberts.com>
2021-03-12 04:04:11 -0500

    Add resources nova group to timezone #39

--- src/Nova/Timezone.php
+++ src/Nova/Timezone.php
@@ -22,6 +22,8 @@ class Timezone extends BaseResource
     public static $search = [
         'id',
     ];
+    
+    public static $group = 'Resources';
 
     public function fieldsForIndex(NovaRequest $request)
     {
