847653450bf4a4e2c68cecdfd13661554b8ce535
devjk1 <jinsik@omniadigital.io>
2021-03-15 11:03:50 -0400

    [TGER-109] Nova State add relationships

--- src/Nova/State.php
+++ src/Nova/State.php
@@ -7,6 +7,7 @@ namespace Tipoff\Addresses\Nova;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\Date;
+use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
@@ -47,6 +48,7 @@ class State extends BaseResource
             Text::make('Description'),
             Text::make('Capital'),
             nova('country') ? BelongsTo::make('Country', 'country', nova('country'))->searchable() : null,
+            nova('zip') ? HasMany::make('Zips', 'zips', nova('zip'))->searchable() : null,
         ]);
     }
 
