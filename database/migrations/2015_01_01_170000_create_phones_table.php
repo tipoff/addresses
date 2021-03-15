6bc95eb344e07cd9bbf3efaed251db156c47712f
drewroberts <github@drewroberts.com>
2021-03-15 14:09:03 -0400

    Update phone migration with area codes #69

--- database/migrations/2015_01_01_170000_create_phones_table.php
+++ database/migrations/2015_01_01_170000_create_phones_table.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Tipoff\Addresses\Models\PhoneArea;
 
 class CreatePhonesTable extends Migration
 {
@@ -16,7 +17,7 @@ class CreatePhonesTable extends Migration
             $table->string('full_number', 15);
 
             // Nullable fields required for US numbers - https://en.wikipedia.org/wiki/North_American_Numbering_Plan
-            $table->string('area_code', 3)->nullable(); // May break out into separate table
+            $table->foreignIdFor(PhoneArea::class)->nullable(); // Does not use ID, field is 'phone_area_code'
             $table->string('exchange_code', 3)->nullable();
             $table->string('line_number', 4)->nullable();
 
