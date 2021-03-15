af058c2cde1dcce8e20c577096a52f6f7e4cf291
devjk1 <jinsik@omniadigital.io>
2021-03-15 10:51:31 -0400

    [TGER-109] Nova City add relationships

--- src/Nova/City.php
+++ src/Nova/City.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Tipoff\Addresses\Nova;
 
 use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsToMany;
 use Laravel\Nova\Fields\Date;
+use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
@@ -37,6 +39,13 @@ class City extends BaseResource
             Text::make('Slug'),
             Text::make('Title'),
             Text::make('Description'),
+            nova('domestic_address') ? HasMany::make('Domestic Addresses', 'domestic addresses', nova('domestic_address'))->searchable() : null,
+            nova('zip') ? BelongsToMany::make('Zips', 'zips', nova('zip'))
+                ->fields(function () {
+                    return [
+                        Text::make('Primary')->default(false),
+                    ];
+                }) : null,
         ]);
     }
 
