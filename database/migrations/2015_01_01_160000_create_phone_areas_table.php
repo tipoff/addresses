1d64aec92fcb2fc950c146ecf501d57099e12c9a
Drew Roberts <github@drewroberts.com>
2021-03-15 22:56:22 -0400

    Resolve state class from service container

--- database/migrations/2015_01_01_160000_create_phone_areas_table.php
+++ database/migrations/2015_01_01_160000_create_phone_areas_table.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Tipoff\Addresses\Models\State;
 
 class CreatePhoneAreasTable extends Migration
 {
@@ -13,7 +12,7 @@ class CreatePhoneAreasTable extends Migration
     {
         Schema::create('phone_areas', function (Blueprint $table) {
             $table->string('code', 3)->index(); // May not start with 1 or 0 - https://en.wikipedia.org/wiki/North_American_Numbering_Plan
-            $table->foreignIdFor(State::class)->nullable();
+            $table->foreignIdFor(app('state'))->nullable();
         });
     }
 }
