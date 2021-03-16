5366262e3e54257b631f987433fab0be87714f6b
Drew Roberts <github@drewroberts.com>
2021-03-15 22:55:40 -0400

    Resolve classes from service container

--- database/migrations/2015_01_01_140000_create_city_zip_table.php
+++ database/migrations/2015_01_01_140000_create_city_zip_table.php
@@ -3,8 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Tipoff\Addresses\Models\City;
-use Tipoff\Addresses\Models\Zip;
 
 class CreateCityZipTable extends Migration
 {
@@ -12,8 +10,8 @@ class CreateCityZipTable extends Migration
     {
         Schema::create('city_zip', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(City::class);
-            $table->foreignIdFor(Zip::class);
+            $table->foreignIdFor(app('city'));
+            $table->foreignIdFor(app('zip'));
             $table->boolean('primary')->default(false);
             $table->timestamps();
         });
