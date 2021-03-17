9ddf814b9914a2aa34fb1c1d4290bdbaa122b236
Drew Roberts <github@drewroberts.com>
2021-03-16 22:21:31 -0400

    Add unique country calling code requirement #67

--- database/migrations/2015_01_01_106000_create_country_callingcodes_table.php
+++ database/migrations/2015_01_01_106000_create_country_callingcodes_table.php
@@ -13,12 +13,14 @@ class CreateCountryCallingcodesTable extends Migration
         Schema::create('country_callingcodes', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(app('country')); // Some countries have more than one Calling Code
-            $table->string('code', 6)->unique(); // https://en.wikipedia.org/wiki/List_of_country_calling_codes
+            $table->string('code', 6); // https://en.wikipedia.org/wiki/List_of_country_calling_codes
             $table->string('display')->nullable(); // Formatted for displaying with a phone number. Ex: "+1 242"
             
             $table->foreignIdFor(app('user'), 'creator_id')->nullable();
             $table->foreignIdFor(app('user'), 'updater_id')->nullable();
             $table->timestamps();
+            
+            $table->unique(['country_id', 'code'], 'callingcode_unique');
         });
     }
 }
