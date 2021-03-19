e18a6daefd7b2136c41858ed7ab8845f5612703d
Drew Roberts <github@drewroberts.com>
2021-03-19 14:43:01 -0400

    Add optional data for us cities #18

--- database/migrations/2015_01_01_130000_create_cities_table.php
+++ database/migrations/2015_01_01_130000_create_cities_table.php
@@ -13,11 +13,22 @@ class CreateCitiesTable extends Migration
             $table->foreignIdFor(app('state'))->index();
             $table->string('slug')->index();
             $table->string('title');
-            
+
+            $table->boolean('incorporated')->default(true);
+            $table->boolean('military')->default(false);
+            $table->boolean('township')->default(false);
+            $table->foreignIdFor(app('timezone'))->nullable();
+            $table->decimal('latitude', 9, 6)->nullable();
+            $table->decimal('longitude', 8, 6)->nullable();
+            $table->tinyInteger('importance')->nullable(); // Ranking of 1-5 to sort cities by importance
+            $table->integer('population')->nullable(); // 2019 census data for urban population
+            $table->integer('population_proper')->nullable(); // 2019 census data for municipal population
+            $table->integer('density')->nullable(); // 2019 census population per square kilometer
+
             $table->foreignIdFor(app('user'), 'creator_id')->nullable();
             $table->foreignIdFor(app('user'), 'updater_id')->nullable();
             $table->timestamps();
-            
+
             $table->unique(['state_id', 'slug'], 'city_unique');
         });
     }
