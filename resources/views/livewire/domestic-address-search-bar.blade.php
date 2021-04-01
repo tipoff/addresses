af87de8c3f1fe67d4d2437b8227ed3fd05065277
devjk1 <jinsik@omniadigital.io>
2021-04-01 15:21:33 -0400

    [TGER-133] add form which contains place details

--- resources/views/livewire/domestic-address-search-bar.blade.php
+++ resources/views/livewire/domestic-address-search-bar.blade.php
@@ -17,4 +17,78 @@
         </div>
         @endforeach
     </div>
+    <form 
+        method="POST"
+        action=""
+        class="w-full"
+    >
+        @csrf
+        <div class="flex justify-between w-full mt-4 text-gray-700">
+            <div class="w-4/12">
+                <label class="ml-2 text-xs text-gray-500 font-semibold tracking-widest">
+                    Address 1
+                </label>
+                <input
+                    id="address-line-1"
+                    name="address-line-1"
+                    type="text"
+                    readonly="readonly"
+                    class="w-full px-2 py-1 bg-yellow-50"
+                >
+            </div>
+            <div class="w-4/12 ml-2">
+                <label class="ml-2 text-xs text-gray-500 font-semibold tracking-widest">
+                    Address 2
+                </label>
+                <input
+                    id="address-line-2"
+                    name="address-line-2"
+                    type="text"
+                    class="w-full px-2 py-1"
+                >
+            </div>
+            <div class="w-2/12 ml-2">
+                <label class="ml-2 text-xs text-gray-500 font-semibold tracking-widest">
+                    City
+                </label>
+                <input
+                    id="city"
+                    name="city"
+                    type="text"
+                    readonly="readonly"
+                    class="w-full px-2 py-1 bg-yellow-50"
+                >
+            </div>
+            <div class="w-1/12 ml-2">
+                <label class="ml-2 text-xs text-gray-500 font-semibold tracking-widest">
+                    State
+                </label>
+                <input
+                    id="state"
+                    name="state"
+                    type="text"
+                    readonly="readonly"
+                    class="w-full px-2 py-1 bg-yellow-50"
+                >
+            </div>
+            <div class="w-1/12 ml-2">
+                <label class="ml-2 text-xs text-gray-500 font-semibold tracking-widest">
+                    Zip
+                </label>
+                <input
+                    id="zip"
+                    name="zip"
+                    type="text"
+                    readonly="readonly"
+                    class="w-full px-2 py-1 bg-yellow-50"
+                >
+            </div>
+        </div>
+        <button 
+            type="submit"
+            class="mt-2 px-2 py-1 bg-white text-green-500 border-2 border-green-400 rounded-md"
+        >
+            Submit
+        </button>
+    </form>
 </div>
