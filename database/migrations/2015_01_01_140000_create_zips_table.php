91964eb589e0269a5564f0bfff63edca2ecad442
Drew Roberts <github@drewroberts.com>
2021-04-02 13:07:45 -0400

    Update migration for zip codes #17

--- database/migrations/2015_01_01_140000_create_zips_table.php
+++ database/migrations/2015_01_01_140000_create_zips_table.php
@@ -10,7 +10,7 @@ class CreateZipsTable extends Migration
     {
         Schema::create('zips', function (Blueprint $table) {
             $table->string('code', 5)->unique()->primary(); // Actual ZIP Code. Has to be string because can have leading zeros. Check model to see how it is made Primary Key.
-            $table->foreignIdFor(app('state'))->nullable();
+            $table->foreignIdFor(app('state'));
             $table->foreignIdFor(app('region'))->nullable();
             $table->foreignIdFor(app('timezone'))->nullable();
 
@@ -20,7 +20,7 @@ class CreateZipsTable extends Migration
             $table->float('longitude', 10, 6)->nullable();
             $table->boolean('military')->default(false);
             $table->boolean('ztca')->default(true);
-	    $table->string('parent_zip_code')->nullable();
+	        $table->string('parent_zip_code')->nullable();
 
             $table->foreignIdFor(app('user'), 'creator_id')->nullable();
             $table->foreignIdFor(app('user'), 'updater_id')->nullable();
