f251607796355d511597e3fd9dc6c73fcd511ab5
devjk1 <jinsik@omniadigital.io>
2021-04-06 03:21:56 -0400

    [TGER-133] register livewire component in Service Provider with package name

--- src/AddressesServiceProvider.php
+++ src/AddressesServiceProvider.php
@@ -72,6 +72,6 @@ class AddressesServiceProvider extends TipoffServiceProvider
     {
         parent::bootingPackage();
 
-        Livewire::component('domestic-address-search-bar', DomesticAddressSearchBar::class);
+        Livewire::component('addresses::domestic-address-search-bar', DomesticAddressSearchBar::class);
     }
 }
