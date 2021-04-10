c00efa76dca78f3cb73f1f86385c720ae504cf32
Drew Roberts <github@drewroberts.com>
2021-04-09 23:43:31 -0400

    Update nova resource for zip #39

--- src/Nova/Zip.php
+++ src/Nova/Zip.php
@@ -40,21 +40,21 @@ class Zip extends BaseResource
     public function fields(Request $request)
     {
         return array_filter([
-            Text::make('Code')->required(),
+            Text::make('Code')->rules('required', 'unique:zips,code', 'max:5'), // @todo Only allow numbers as acceptable characters
             nova('state') ? BelongsTo::make('State', 'state', nova('state'))->searchable() : null,
             nova('region') ? BelongsTo::make('Region', 'region', nova('region'))->searchable() : null,
             nova('timezone') ? BelongsTo::make('Timezone', 'timezone', nova('timezone'))->searchable() : null,
             Number::make('Latitude')->step(0.000001)->nullable(),
             Number::make('Longitude')->step(0.000001)->nullable(),
             Boolean::make('Decommissioned')->default(0),
-            /* @todo HasMany::searchable does not exist  */
-            /*nova('domestic_address') ? HasMany::make('Domestic Address', 'domestic address', nova('domestic_address'))->searchable() : null,*/
+
             nova('city') ? BelongsToMany::make('Cities', 'cities', nova('city'))
                 ->fields(function () {
                     return [
                         Text::make('Primary')->default(false),
                     ];
                 }) : null,
+            nova('domestic_address') ? HasMany::make('Domestic Address', 'domestic address', nova('domestic_address')) : null,
         ]);
     }
 
