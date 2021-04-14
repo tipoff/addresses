cd9bf705ff21eadc4feb990cb929deaba2c466e8
wolfrednicolas <wolfrednicolas@users.noreply.github.com>
2021-04-14 22:02:12 +0000

    Fix styling

--- src/Nova/Address.php
+++ src/Nova/Address.php
@@ -35,7 +35,7 @@ class Address extends BaseResource
             Text::make('Company')->sortable(),
             Text::make('Extended Zip')->sortable(),
             Text::make('Phone', 'phone.id', function () {
-                if(!empty($this->phone->full_number)){
+                if (! empty($this->phone->full_number)) {
                     return $this->phone->full_number;
                 }
             })->sortable(),
