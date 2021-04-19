eb2eb757f923f5458fe954f448e5d31a02d88367
devjk1 <jinsik@omniadigital.io>
2021-04-19 16:37:34 -0400

    [TGER-140] separate component into two; search bar & fields

--- resources/views/domestic-address-search-bar-javascript.blade.php
+++ resources/views/domestic-address-search-bar-javascript.blade.php
@@ -1,83 +1,10 @@
-<div class="w-3/4">
+<div class="w-full">
     <input
         id="autocomplete"
         type="text"
-        class="w-full px-2 py-1"
+        placeholder="Enter your address"
+        class="w-full px-2 py-1 focus:outline-none text-gray-700 ring-1 ring-gray-300 rounded-md overflow-hidden focus:ring-2 focus:ring-blue-300"
     >
-    <form 
-        method="POST"
-        action=""
-        class="w-full"
-    >
-        @csrf
-        <div class="flex justify-between w-full mt-4 text-gray-700">
-            <div class="w-4/12">
-                <label class="ml-2 text-xs text-gray-500 font-semibold tracking-widest">
-                    Address 1
-                </label>
-                <input
-                    id="address-line-1"
-                    name="address-line-1"
-                    type="text"
-                    readonly="readonly"
-                    class="w-full px-2 py-1 bg-yellow-50"
-                >
-            </div>
-            <div class="w-4/12 ml-2">
-                <label class="ml-2 text-xs text-gray-500 font-semibold tracking-widest">
-                    Address 2
-                </label>
-                <input
-                    id="address-line-2"
-                    name="address-line-2"
-                    type="text"
-                    class="w-full px-2 py-1"
-                >
-            </div>
-            <div class="w-2/12 ml-2">
-                <label class="ml-2 text-xs text-gray-500 font-semibold tracking-widest">
-                    City
-                </label>
-                <input
-                    id="city"
-                    name="city"
-                    type="text"
-                    readonly="readonly"
-                    class="w-full px-2 py-1 bg-yellow-50"
-                >
-            </div>
-            <div class="w-1/12 ml-2">
-                <label class="ml-2 text-xs text-gray-500 font-semibold tracking-widest">
-                    State
-                </label>
-                <input
-                    id="state"
-                    name="state"
-                    type="text"
-                    readonly="readonly"
-                    class="w-full px-2 py-1 bg-yellow-50"
-                >
-            </div>
-            <div class="w-1/12 ml-2">
-                <label class="ml-2 text-xs text-gray-500 font-semibold tracking-widest">
-                    Zip
-                </label>
-                <input
-                    id="zip"
-                    name="zip"
-                    type="text"
-                    readonly="readonly"
-                    class="w-full px-2 py-1 bg-yellow-50"
-                >
-            </div>
-        </div>
-        <button 
-            type="submit"
-            class="mt-2 px-2 py-1 bg-white text-green-500 border-2 border-green-400 rounded-md"
-        >
-            Submit
-        </button>
-    </form>
 </div>
 
 @push ('scripts')
