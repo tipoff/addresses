dd227554626f5e3ed74b4fabb525348ab229e79f
Patrick Breen <patrick.breen@quartzy.com>
2021-04-23 12:07:31 -0400

    Add scaffolding for custom field within package

---
+++ src/Nova/Fields/DomesticAddress.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Addresses\Nova\Fields;
+
+use Laravel\Nova\Fields\Field;
+
+class DomesticAddress extends Field
+{
+    public $component = 'domestic-address';
+}
